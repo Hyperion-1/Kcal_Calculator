c471ec752b221751b7639be9320743c3d1715b04
Brad <walker.brad26@gmail.com>
2021-11-26 23:04:24 -0600

    smol updates

--- script.js
+++ script.js
@@ -7,28 +7,7 @@
 *  Women - 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) – 161   
 *
 */
-/*
-const submit = document.querySelectorAll('#submit');
-submit.onclick = function gender(){
 
-let male = document.getElementById('#maleButton');
-let female = document.getElementById('#femaleButton');
-
-    if(male.checked==true){
-      console.log(mensCal());
-     // alert('male selected');
-
-    } else if(female.checked==true){
-        womenCal();
-       //alert('female selected');
-
-    } else{
-        alert('please select a gender');
-    }
-
-
-    }//end gender()
-*/
 
 
 
@@ -62,3 +41,25 @@ function womenCal(){
     return base
 }
 
+/*
+const submit = document.querySelectorAll('#submit');
+submit.onclick = function gender(){
+
+let male = document.getElementById('#maleButton');
+let female = document.getElementById('#femaleButton');
+
+    if(male.checked==true){
+      console.log(mensCal());
+     // alert('male selected');
+
+    } else if(female.checked==true){
+        womenCal();
+       //alert('female selected');
+
+    } else{
+        alert('please select a gender');
+    }
+
+
+    }//end gender()
+*/
